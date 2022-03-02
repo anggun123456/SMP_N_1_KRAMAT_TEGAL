d25d67a46818c780e9405c27be2d8641274a6681
anggun123456 <332anggunseptiana@gmail.com>
2022-03-02 10:22:44 +0700

    commit2

--- app/src/main/java/com/example/smpn1kramattegal/MainActivity.java
+++ app/src/main/java/com/example/smpn1kramattegal/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.smpn1kramattegal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void guru(View view) {
+    }
+
+    public void eskul(View view) {
+    }
+
+    public void profil(View view) {
+    }
+
+    public void fasilitas(View view) {
+    }
+
+    public void perpus(View view) {
+    }
+
+    public void galeri(View view) {
     }
 }
